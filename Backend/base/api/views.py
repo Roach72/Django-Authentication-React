578ffb757fa07bd99136f95bd38db7b6bdbc12de
roshdy <roshdysaid72@gmail.com>
2024-02-04 16:52:58 +0200

    first commit

---
+++ Backend/base/api/views.py
@@ -0,0 +1,47 @@
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
+# Customizing token claims
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+from .serializers import NoteSerializer
+from base.models import Note
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        'api/token/refresh',
+    ]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializers = NoteSerializer(notes, many=True)
+    return Response(serializers.data)
